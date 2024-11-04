6f4bde3dc73db48c42762545e32e65d54e0c2e09
Ella Raputri <ellaraputri@gmail.com>
2024-11-04 23:32:51 +0700

    register page ui done

--- code/login.py
+++ code/login.py
@@ -20,62 +20,61 @@ class LoginPage(tk.Frame):
 
 
         # background
-        background_photo = tk.PhotoImage(file='images/login_bg.png')
-        self.background_photo = background_photo  
-        background_label = tk.Label(self.master, image=self.background_photo)
+        self.background_photo = tk.PhotoImage(file='images/login_bg.png')  
+        background_label = tk.Label(self, image=self.background_photo)  
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         # title
-        title_label = tk.Label(self.master, text="Welcome!", font=("Poppins", 44, "bold"), fg=self.master.font_color1, bg='white')
+        title_label = tk.Label(self, text="Welcome!", font=("Poppins", 44, "bold"), fg=self.master.font_color1, bg='white')
         title_label.place(x=879, y=40)
 
         # instruction
-        instruction_label = tk.Label(self.master, text="Please login to your account before continue.",
+        instruction_label = tk.Label(self, text="Please login to your account before continue.",
                                      wraplength=682, font=("Poppins", 24), fg=self.master.font_color1, 
                                      bg='white', justify='center')
         instruction_label.place(x=735, y=132)
         
         # email field
-        email_label = tk.Label(self.master, text="Email",font=("Poppins Semibold", 20), 
+        email_label = tk.Label(self, text="Email",font=("Poppins Semibold", 20), 
                                fg=self.master.font_color1, bg='white')
         email_label.place(x=730, y=257)
 
-        email_entry = tk.Entry(self.master, font=('Poppins',18), width=38,
+        email_entry = tk.Entry(self, font=('Poppins',18), width=38,
                         highlightcolor=self.master.font_color1,
                         highlightbackground='grey',highlightthickness=2,
                         bd=0, bg='white')
         email_entry.place(x=735, y=304)
 
         # password field
-        password_label = tk.Label(self.master, text="Password",font=("Poppins Semibold", 20), 
+        password_label = tk.Label(self, text="Password",font=("Poppins Semibold", 20), 
                                fg=self.master.font_color1, bg='white')
         password_label.place(x=730, y=400)
 
-        password_entry = tk.Entry(self.master, font=('Poppins',18), width=38,
+        password_entry = tk.Entry(self, font=('Poppins',18), width=38,
                         highlightcolor=self.master.font_color1,
                         highlightbackground='grey',highlightthickness=2,
                         bd=0, bg='white', show='*')
         password_entry.place(x=735, y=448)
 
-        show_hide_btn = tk.Button(self.master, image=self.show_pass_img, 
+        show_hide_btn = tk.Button(self, image=self.show_pass_img, 
                             bd=0, command=show_hide_password, bg='white')
         show_hide_btn.place(x=1260, y=460)
 
 
         # login button
-        login_btn = tk.Button(self.master, text='Login', bg=self.master.bg_color1,
+        login_btn = tk.Button(self, text='Login', bg=self.master.bg_color1,
                                      fg='white', font=('Poppins Semibold', 24), bd=0)
         login_btn.place(x=895, y=560, width=258, height=74)
 
 
         # sign up label
-        signup_label = tk.Label(self.master, text="Don't have an account yet?",
+        signup_label = tk.Label(self, text="Don't have an account yet?",
                                      font=("Poppins", 16), fg=self.master.font_color1, 
                                      bg='white', justify='center')
         signup_label.place(x=800, y=670)
 
         # here label
-        here_label = tk.Label(self.master, text="Sign up here",
+        here_label = tk.Label(self, text="Sign up here",
                                      font=("Poppins", 16, 'underline'), fg=self.master.font_color1, 
                                      bg='white', justify='center', cursor="hand2")
         here_label.place(x=1101, y=670)
--- code/register.py
+++ code/register.py
@@ -2,58 +2,180 @@ import tkinter as tk
 
 class RegisterPage(tk.Frame):
     def __init__(self, parent):
-        super().__init__(parent, highlightbackground=parent.font_color1,
-                         highlightthickness=3)
-        self.configure(width=400, height=420)
+        super().__init__(parent)
+        self.master = parent
+        self.show_pass_img = tk.PhotoImage(file='images/show_pass.png')
+        self.hide_pass_img = tk.PhotoImage(file='images/hide_pass.png')
         self.create_widgets()
+    
 
     def create_widgets(self):
-        heading_label = tk.Label(self, text="Welcomessss to System",
-                                  fg='white', bg=self.master.font_color1, font=('Bold', 18))
-        heading_label.place(x=0, y=0, width=400)
+        def show_hide_password():
+            if password_entry['show'] == '*':
+                password_entry.config(show='')
+                show_hide_btn.config(image=self.hide_pass_img)
+            else:
+                password_entry.config(show='*')
+                show_hide_btn.config(image=self.show_pass_img)
+        
+        def show_hide_confirm_password():
+            if confirm_password_entry['show'] == '*':
+                confirm_password_entry.config(show='')
+                confirm_show_hide_btn.config(image=self.hide_pass_img)
+            else:
+                confirm_password_entry.config(show='*')
+                confirm_show_hide_btn.config(image=self.show_pass_img)
 
-    #     heading_label = tk.Label(self, text="Student Login Page",
-    #                               fg='white', bg=self.master.bg_color, font=('Bold', 18))
-    #     heading_label.place(x=0, y=0, width=400)
 
-    #     stud_icon_label = tk.Label(self, image=self.master.login_stud_icon)
-    #     stud_icon_label.place(x=150, y=40)
 
-    #     id_num_label = tk.Label(self, text='Enter Student ID Number:',
-    #                              font=('Bold', 15), fg=self.master.bg_color)
-    #     id_num_label.place(x=80, y=140)
+        # background
+        self.background_photo = tk.PhotoImage(file='images/signup_bg.png')  
+        background_label = tk.Label(self, image=self.background_photo)  
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    #     self.id_num_ent = tk.Entry(self, font=('Bold', 15), justify=tk.CENTER,
-    #                                highlightcolor=self.master.bg_color,
-    #                                highlightbackground='gray', highlightthickness=2)
-    #     self.id_num_ent.place(x=80, y=190)
+        # title
+        title_label = tk.Label(self, text="Register", font=("Poppins", 40, "bold"), fg=self.master.font_color1, bg='white')
+        title_label.place(x=111, y=85)
 
-    #     pass_label = tk.Label(self, text='Enter Password:',
-    #                           font=('Bold', 15), fg=self.master.bg_color)
-    #     pass_label.place(x=80, y=240)
+        # instruction
+        instruction_label = tk.Label(self, text="Please fill in the required information.",
+                                     font=("Poppins", 16), fg=self.master.font_color1, 
+                                     bg='white')
+        instruction_label.place(x=111, y=163)
+        
+        # email field
+        email_label = tk.Label(self, text="Email",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        email_label.place(x=111, y=232)
 
-    #     self.pass_ent = tk.Entry(self, font=('Bold', 15), justify=tk.CENTER,
-    #                              highlightcolor=self.master.bg_color,
-    #                              highlightbackground='gray', highlightthickness=2,
-    #                              show='*')
-    #     self.pass_ent.place(x=80, y=290)
+        email_entry = tk.Entry(self, font=('Poppins',16), width=39,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white')
+        email_entry.place(x=111, y=273)
 
-    #     self.show_hide_btn = tk.Button(self, image=self.master.locked_icon,
-    #                                     bd=0, command=self.show_hide_password)
-    #     self.show_hide_btn.place(x=310, y=280)
 
-    #     login_btn = tk.Button(self, text='Login',
-    #                           font=('Bold', 15), bg=self.master.bg_color, fg='white')
-    #     login_btn.place(x=95, y=340, width=200, height=40)
+        # password field
+        password_label = tk.Label(self, text="Password",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        password_label.place(x=111, y=359)
 
-    #     forget_pass_btn = tk.Button(self, text='⚠️\nForget Password',
-    #                                  fg=self.master.bg_color, bd=0)
-    #     forget_pass_btn.place(x=150, y=390)
+        password_entry = tk.Entry(self, font=('Poppins',16), width=39,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white', show='*')
+        password_entry.place(x=111, y=401)
 
-    # def show_hide_password(self):
-    #     if self.pass_ent['show'] == '*':
-    #         self.pass_ent.config(show='')
-    #         self.show_hide_btn.config(image=self.master.unlocked_icon)
-    #     else:
-    #         self.pass_ent.config(show='*')
-    #         self.show_hide_btn.config(image=self.master.locked_icon)
+        show_hide_btn = tk.Button(self, image=self.show_pass_img, 
+                            bd=0, command=show_hide_password, bg='white')
+        show_hide_btn.place(x=565, y=412)
+
+
+        # confirm password field
+        confirm_password_label = tk.Label(self, text="Confirm Password",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        confirm_password_label.place(x=111, y=494)
+
+        confirm_password_entry = tk.Entry(self, font=('Poppins',16), width=39,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white', show='*')
+        confirm_password_entry.place(x=111, y=532)
+
+        confirm_show_hide_btn = tk.Button(self, image=self.show_pass_img, 
+                            bd=0, command=show_hide_confirm_password, bg='white')
+        confirm_show_hide_btn.place(x=565, y=544)
+
+
+        # login label
+        login_label = tk.Label(self, text="Already have an account?",
+                                     font=("Poppins", 16), fg=self.master.font_color1, 
+                                     bg='white', justify='center')
+        login_label.place(x=136, y=645)
+
+        # here label
+        here_label = tk.Label(self, text="Login here",
+                                     font=("Poppins", 16, 'underline'), fg=self.master.font_color1, 
+                                     bg='white', justify='center', cursor="hand2")
+        here_label.place(x=415, y=645)
+        here_label.bind("<Button-1>", self.click_label_login)
+
+
+        # fname field
+        fname_label = tk.Label(self, text="First Name",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        fname_label.place(x=679, y=102)
+
+        fname_entry = tk.Entry(self, font=('Poppins',16), width=19,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white')
+        fname_entry.place(x=679, y=145)
+
+
+        # lname field
+        lname_label = tk.Label(self, text="Last Name",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        lname_label.place(x=978, y=102)
+
+        lname_entry = tk.Entry(self, font=('Poppins',16), width=19,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white')
+        lname_entry.place(x=978, y=145)
+
+        # phone field
+        phone_label = tk.Label(self, text="Phone Number",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        phone_label.place(x=679, y=230)
+
+        phone_entry = tk.Entry(self, font=('Poppins',16), width=42,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white')
+        phone_entry.place(x=679, y=273)
+
+
+        # dob field
+        dob_label = tk.Label(self, text="Date of Birth",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        dob_label.place(x=679, y=358)
+
+        dob_entry = tk.Entry(self, font=('Poppins',16), width=19,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white')
+        dob_entry.place(x=679, y=401)
+
+
+        # city field
+        city_label = tk.Label(self, text="Last Name",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        city_label.place(x=978, y=358)
+
+        city_entry = tk.Entry(self, font=('Poppins',16), width=19,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white')
+        city_entry.place(x=978, y=401)
+
+        # address field
+        address_label = tk.Label(self, text="Address Details",font=("Poppins Semibold", 18), 
+                               fg=self.master.font_color1, bg='white')
+        address_label.place(x=679, y=494)
+
+        address_entry = tk.Entry(self, font=('Poppins',16), width=42,
+                        highlightcolor=self.master.font_color1,
+                        highlightbackground='grey',highlightthickness=2,
+                        bd=0, bg='white')
+        address_entry.place(x=679, y=532)
+
+
+        # submit btn
+        submit_btn = tk.Button(self, text='Submit', bg=self.master.bg_color1,
+                                     fg='white', font=('Poppins Semibold', 18), bd=0)
+        submit_btn.place(x=1120, y=635, width=128, height=49)
+    
+
+    def click_label_login(self, event):
+        self.master.show_frame(self.master.login_page)
