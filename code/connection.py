529b4c0ff830256ded1b5427187e3b7ad953ec79
Ella Raputri <ellaraputri@gmail.com>
2024-12-11 23:29:08 +0700

    Update connection.py

--- code/connection.py
+++ code/connection.py
@@ -5,7 +5,7 @@ def connect_to_db():
         connection = mysql.connector.connect(
             host="localhost",        
             user="root",             
-            password="Ella84712005#",  
+            password="EllaTest2005",  
             database="ClinicSystemDB",   
             port=3306                
         )
